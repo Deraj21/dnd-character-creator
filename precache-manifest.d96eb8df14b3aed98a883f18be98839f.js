0c214c60d920b16688662a7fdc17368e800ba273
Jared Tanner <Deraj21@gmail.com>
2019-04-16 19:05:11 -0700

    Updates

---
+++ precache-manifest.d96eb8df14b3aed98a883f18be98839f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dnd-character-creator/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b4ec95d3e75460a8d798",
+    "url": "/dnd-character-creator/static/js/runtime~main.e8783143.js"
+  },
+  {
+    "revision": "3aeaa7d5e322b4640993",
+    "url": "/dnd-character-creator/static/js/main.43413504.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/dnd-character-creator/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "3aeaa7d5e322b4640993",
+    "url": "/dnd-character-creator/static/css/main.3cbec852.chunk.css"
+  },
+  {
+    "revision": "9c668a6453601d2aef69e9aff445e42d",
+    "url": "/dnd-character-creator/index.html"
+  }
+];
